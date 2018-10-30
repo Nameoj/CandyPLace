f9f9bf074327df0a52cab3c21983ec9385718df1
Hugo Hontans <hugohontans@gmail.com>
2018-10-30 21:35:21 +0100

    navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,32 +3,28 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { CollectionComponent } from './collection/collection.component';
-<<<<<<< HEAD
 import { ChasseComponent } from './chasse/chasse.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { VitrineComponent } from './vitrine/vitrine.component';
 
 const appRoutes: Routes = [
 {path: 'hunt', component: ChasseComponent},
 {path: 'collection', component: CollectionComponent},
+{path: 'showcase', component: VitrineComponent},
 { path: '',
     redirectTo: '/collection',
     pathMatch: 'full'
   }
 ];
-=======
-import { VitrineComponent } from './vitrine/vitrine.component';
->>>>>>> 0f3fce4155e411e717689ab74a1a4c2bc3a5b12a
+
 
 @NgModule({
   declarations: [
     AppComponent,
     CollectionComponent,
-<<<<<<< HEAD
     ChasseComponent,
-    NavbarComponent
-=======
+    NavbarComponent,
     VitrineComponent
->>>>>>> 0f3fce4155e411e717689ab74a1a4c2bc3a5b12a
   ],
   imports: [
     RouterModule.forRoot(appRoutes),
