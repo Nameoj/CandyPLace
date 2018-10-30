b35a80963fde6c7511ed84f09ae5854695199c39
Julien sch <julien.schader@laposte.net>
2018-10-30 21:46:28 +0100

    appel api et ajout component candyLog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { CandyLogComponent } from './candy-log/candy-log.component';
+import { BonbonlogService } from './bonbonlog.service';
+import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CandyLogComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    BonbonlogService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
