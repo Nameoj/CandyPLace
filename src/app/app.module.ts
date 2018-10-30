6af51bb604c2c2aeb6edc7b7ee167d0dcd7ba61d
Hugo Hontans <hugohontans@gmail.com>
2018-10-30 20:06:09 +0100

    creation du component chasse

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CollectionComponent } from './collection/collection.component';
+import { ChasseComponent } from './chasse/chasse.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CollectionComponent
+    CollectionComponent,
+    ChasseComponent
   ],
   imports: [
     BrowserModule
