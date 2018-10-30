665a01bcefb1a8b0442cf6d5160dea11512bd5bc
Julien Dcode <juliendcode@gmail.com>
2018-10-30 21:38:51 +0100

    profile component and page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CollectionComponent } from './collection/collection.component';
+import { ProfilepageComponent } from './profilepage/profilepage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CollectionComponent
+    CollectionComponent,
+    ProfilepageComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/profilepage/profilepage.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { ProfilesService } from './../services/profiles.service';
+
+@Component({
+  selector: 'app-profilepage',
+  templateUrl: './profilepage.component.html',
+  styleUrls: ['./profilepage.component.css']
+})
+export class ProfilepageComponent implements OnInit {
+  Users;
+  varUser = 0;
+  constructor(private profilesservices: ProfilesService) {}
+
+  ngOnInit() {
+    this.Users = this.profilesservices.Users;
+    console.log(this.Users);
+  }
+}
