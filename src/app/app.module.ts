c4bc9e4a882d471cb98bafb68e15f1d5fc7f4af3
Hugo Hontans <hugohontans@gmail.com>
2018-10-30 21:12:38 +0100

    navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { CollectionComponent } from './collection/collection.component';
 import { ChasseComponent } from './chasse/chasse.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+const appRoutes: Routes = [
+{path: 'hunt', component: ChasseComponent},
+{path: 'collection', component: CollectionComponent},
+{ path: '',
+    redirectTo: '/collection',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     CollectionComponent,
-    ChasseComponent
+    ChasseComponent,
+    NavbarComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule
   ],
   providers: [],
