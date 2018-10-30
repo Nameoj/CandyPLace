7397cb6888ac89f4445a4d170358ef8b863a48fe
Hugo Hontans <hugohontans@gmail.com>
2018-10-30 16:00:13 +0100

    ajout des 7 profiles

--- src/app/services/profiles.service.ts
+++ src/app/services/profiles.service.ts
@@ -78,6 +78,190 @@ export class ProfilesService {
 						quantity : 5
 					}
 				]			
+			},
+			{
+				profile : {
+					firstname : 'Simon',
+					lastname : 'Barbu',
+					adress : '59 Cours Suchet, 69002 Lyon',
+					photo : 'https://avatars0.githubusercontent.com/u/28387526?s=460&v=4'
+				},
+				collection : [
+					{
+						name : 'Kit Kat',
+						quantity : 4
+					},				
+					{
+						name : 'Cerise à la liqueur',
+						quantity : 15
+					},
+					{
+						name : 'Ferrero Rocher',
+						quantity : 2
+					},
+					{
+						name : 'Ferrero Rocher Heart Box 4 Pack',
+						quantity : 1
+					}
+				],
+				vitrine : [
+					{
+						name : 'Ferrero Rocher Heart Box 4 Pack',
+						quantity : 48
+					},
+					{
+						name : 'Chocolat Noir fourré à la liqueur',
+						quantity : 13
+					}
+				]			
+			},
+			{
+				profile : {
+					firstname : 'Julien',
+					lastname : 'Chader',
+					adress : '61 Cours Suchet, 69002 Lyon',
+					photo : 'https://avatars3.githubusercontent.com/u/42383829?s=460&v=4'
+				},
+				collection : [
+				],
+				vitrine : [
+					{
+						name : 'Ferrero Rocher Heart Box 4 Pack',
+						quantity : 48
+					},
+					{
+						name : 'Chocolat Noir fourré à la liqueur',
+						quantity : 13
+					},
+					{
+						name : 'Kit Kat',
+						quantity : 56
+					},				
+					{
+						name : 'Cerise à la liqueur',
+						quantity : 46
+					}
+				]			
+			},
+			{
+				profile : {
+					firstname : 'Julien',
+					lastname : 'Chevrot',
+					adress : '64 Cours Suchet, 69002 Lyon',
+					photo : 'https://avatars3.githubusercontent.com/u/40598218?s=460&v=4'
+				},
+				collection : [
+					{
+						name : 'Kit Kat',
+						quantity : 2
+					},				
+					{
+						name : 'Cerise à la liqueur',
+						quantity : 1
+					},
+					{
+						name : 'Ferrero Rocher',
+						quantity : 2
+					},
+					{
+						name : 'Ferrero Rocher Heart Box 4 Pack',
+						quantity : 1
+					},
+					{
+						name : 'Avello Rochers',
+						quantity : 1
+					},				
+					{
+						name : 'Mon cheri',
+						quantity : 3
+					},
+					{
+						name : 'Mon Chéri Stern',
+						quantity : 2
+					},
+					{
+						name : 'Chocolat Noir fourré à la liqueur',
+						quantity : 1
+					}
+				],
+				vitrine : [
+				]			
+			},
+			{
+				profile : {
+					firstname : 'Elizaveta',
+					lastname : 'Patrusheva',
+					adress : '66 Cours Suchet, 69002 Lyon',
+					photo : 'https://avatars2.githubusercontent.com/u/42244315?s=460&v=4'
+				},
+				collection : [
+					{
+						name : 'Kit Kat',
+						quantity : 2
+					},				
+					{
+						name : 'Cerise à la liqueur',
+						quantity : 1
+					},
+					{
+						name : 'Ferrero Rocher',
+						quantity : 2
+					}
+				],
+				vitrine : [
+					{
+						name : 'Mon cheri',
+						quantity : 67
+					},
+					{
+						name : 'Mon Chéri Stern',
+						quantity : 45
+					}
+				]			
+			},
+			{
+				profile : {
+					firstname : 'Jerzy',
+					lastname : 'Rastapopulos',
+					adress : '67 Cours Suchet, 69002 Lyon',
+					photo : 'https://avatars2.githubusercontent.com/u/43041194?s=460&v=4'
+				},
+				collection : [
+					{
+						name : 'Kit Kat',
+						quantity : 3
+					},
+					{
+						name : 'Lindor 70% Cacao',
+						quantity : 5
+					},
+					{
+						name : 'Cerise à la liqueur',
+						quantity : 1
+					},
+					{
+						name : 'Ferrero Rocher',
+						quantity : 1
+					},
+					{
+						name : 'Avello Rochers',
+						quantity : 2
+					},
+					{
+						name : 'Grand Ferrero Rocher',
+						quantity : 4
+					}
+				],
+				vitrine : [
+					{
+						name : 'Mon cheri',
+						quantity : 45
+					},
+					{
+						name : 'Mon Chéri Stern',
+						quantity : 90
+					}
+				]			
 			}
 		]
 	
