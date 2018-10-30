c8319f0c0d9c52b281364d61411bebd5a97fff00
Hugo Hontans <hugohontans@gmail.com>
2018-10-30 22:35:19 +0100

    rendu propre

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-import { CollectionComponent } from './collection/collection.component';
 import { CandyLogComponent } from './candy-log/candy-log.component';
 import { BonbonlogService } from './bonbonlog.service';
 import { HttpModule } from '@angular/http';
@@ -28,14 +27,12 @@ const appRoutes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    navbarmenu,
     CandyLogComponent,
     AppComponent,
     CollectionComponent,
     ChasseComponent,
     NavbarComponent,
-    VitrineComponent
-    CollectionComponent,
+    VitrineComponent,
     CandyLogComponent
   ],
   imports: [
