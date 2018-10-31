bae7478ed080ef6822bcad3869d67069f22235bc
Zakaria Hamichi <Hamichi.zakaria@gmail.com>
2018-10-31 13:58:27 +0100

    grisage

---
+++ .history/src/app/candy-log/candy-log.component_20181031105957.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from "@angular/core";
+import { BonbonlogService } from "../bonbonlog.service";
+import { ActivatedRoute, ParamMap } from "@angular/router";
+import { ProfilesService } from "./../services/profiles.service";
+
+@Component({
+  selector: "app-candy-log",
+  templateUrl: "./candy-log.component.html",
+  styleUrls: ["./candy-log.component.css"]
+})
+export class CandyLogComponent implements OnInit {
+  objectBonbon: object[];
+  affichage: boolean = false;
+  Users;
+
+  constructor(
+    private service: BonbonlogService,
+    private profilesservices: ProfilesService
+  ) {}
+
+  apiBonbec() {
+    this.service.apiCall().subscribe((data: any) => {
+      this.objectBonbon = data;
+      this.affichage = true;
+      console.log(this.objectBonbon);
+    });
+  }
+
+  ngOnInit() {
+    this.apiBonbec();
+    this.Users = this.profilesservices.Users;
+  }
+
+  grisageCandyLog(){
+
+    for(i = 0; i < 7)
+    if (this.Users.collection.name == this.objectBonbon.product_name_fr){
+
+    }
+  }
+  
+
+
+}
